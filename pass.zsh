8d464611491634c5c8ba289b0f2ec23215cb67bd
Jesse N <jesse@keplerdev.com>
2022-02-13 16:51:18 -0500

    updated compdef
    Signed-off-by: Jesse N <jesse@keplerdev.com>

--- pass.zsh
+++ pass.zsh
@@ -1,21 +1,10 @@
 #!/usr/bin/env zsh
 
+# shellcheck disable=SC2312,SC2296,SC3043,SC2155,SC2250
+
 if ! builtin command -v pass >/dev/null; then
     return 0;
 fi
 
-main() {
-    set -o errexit;
-    set -o pipefail;
-    set -o nounset;
-
-    local -r __dirname="$(cd "$(dirname "${(%):-%x}")" && pwd)";
-    local -r __filename="${__dirname}/$(basename "${(%):-%x}")";
-
-    compdef _pass pass;
-    zstyle ':completion::complete:pass::' prefix "$HOME/.password-store/";
-
-    exit 0;
-};
-
-main "$@";
+local -r __dirname="$(cd "$(dirname "${(%):-%x}")" && pwd)";
+compdef "$__dirname/_pass" pass;
