7e804118b8d2dd874d0c86e8f8a5410093e76471
Jesse N <jesse@keplerdev.com>
2022-02-13 14:09:22 -0500

    updated zsh compdef

--- pass.zsh
+++ pass.zsh
@@ -3,20 +3,21 @@
 #!/usr/bin/env zsh
 
 if ! builtin command -v pass >/dev/null; then
-  return 0;
+    return 0;
 fi
 
 main() {
-  set -o errexit;
-  set -o pipefail;
-  set -o nounset;
+    set -o errexit;
+    set -o pipefail;
+    set -o nounset;
 
-  local -r __dirname="$(cd "$(dirname "${(%):-%x}")" && pwd)";
-  local -r __filename="${__dirname}/$(basename "${(%):-%x}")";
+    local -r __dirname="$(cd "$(dirname "${(%):-%x}")" && pwd)";
+    local -r __filename="${__dirname}/$(basename "${(%):-%x}")";
 
-  source <(cat "$__dirname/_pass");
+    compdef _pass pass;
+    zstyle ':completion::complete:pass::' prefix "$HOME/.password-store/";
 
-  exit 0;
+    exit 0;
 };
 
 main "$@";
